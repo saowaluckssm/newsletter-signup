25dce1b2406e5d399d061f4d886b1474225cb781
Saowaluck Srikrishna <saowalucksrikrishna@Saowalucks-MBP.fios-router.home>
2020-12-07 13:08:05 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us7.api.mailchimp.com/3.0/lists/92c157acca"
+
+  const options = {
+    method: "POST",
+    auth: "saowaluck:cc7c82a0047b67871c8f35388c9c368e-us7"
+
+  }
+
+  const request = https.request(url, options, function(response){
+
+    if(response.statusCode === 200){
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+
+  })
+
+  request.write(jsonData);
+  request.end();
+
+
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+})
+
+
+app.listen( process.env.PORT || 3000, function(){
+  console.log("Server started on port 3000");
+});
+
+
+
+// API key
+
+// cc7c82a0047b67871c8f35388c9c368e-us7
+
+
+// List id
+
+// 92c157acca
